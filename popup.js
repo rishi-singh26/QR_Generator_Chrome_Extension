6de7b33c44af882542bb96897a65ab10b320fe5b
rishi <rishisingh0831@gmail.com>
2021-02-01 21:41:53 +0530

    Initial commit

---
+++ popup.js
@@ -0,0 +1,19 @@
+var qrcode = new QRCode("qrcode");
+
+function makeCode() {
+  var elText = document.getElementById("text");
+
+  if (!elText.value) {
+    alert("Input a text");
+    elText.focus();
+    return;
+  }
+
+  qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+document.getElementById("text").addEventListener("input", (event) => {
+  makeCode();
+});
